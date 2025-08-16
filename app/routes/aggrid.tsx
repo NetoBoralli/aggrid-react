ab48c80a73ad80dc5a1e2cb16275a67dd81bb03e
Nelson Boralli Neto <netoboralli@gmail.com>
2025-08-16 15:59:48 -0300

    Basic tutorial example

--- app/routes/aggrid.tsx
+++ app/routes/aggrid.tsx
@@ -1,6 +1,17 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import type { Route } from "./+types/home";
 import { AgGridReact } from "ag-grid-react";
+
+interface SpaceMission {
+  mission: string;
+  company: string;
+  location: string;
+  date: string;
+  time: string;
+  rocket: string;
+  price: number;
+  successful: boolean;
+}
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,34 +20,93 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+const CompanyLogoRenderer = ({ value }: { value: string }) => (
+  <span
+    style={{
+      display: "flex",
+      height: "100%",
+      width: "100%",
+      alignItems: "center",
+    }}
+  >
+    {value && (
+      <img
+        alt={`${value} Flag`}
+        src={`https://www.ag-grid.com/example-assets/space-company-logos/${value.toLowerCase()}.png`}
+        style={{
+          display: "block",
+          width: "25px",
+          height: "auto",
+          maxHeight: "50%",
+          marginRight: "12px",
+          filter: "brightness(1.1)",
+        }}
+      />
+    )}
+    <p
+      style={{
+        textOverflow: "ellipsis",
+        overflow: "hidden",
+        whiteSpace: "nowrap",
+      }}
+    >
+      {value}
+    </p>
+  </span>
+);
+
 export default function AgGrid() {
   const GridExample = () => {
-    const [rowData, setRowData] = useState([
-      { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-      { make: "Ford", model: "F-Series", price: 33850, electric: false },
-      { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-      { make: "Mercedes", model: "EQA", price: 48890, electric: true },
-      { make: "Fiat", model: "500", price: 15774, electric: false },
-      { make: "Nissan", model: "Juke", price: 20675, electric: false },
-    ]);
-  
+    const [rowData, setRowData] = useState<SpaceMission[]>([]);
+
     const [colDefs, setColDefs] = useState([
-      { field: "make" as const },
-      { field: "model" as const },
-      { field: "price" as const },
-      { field: "electric" as const },
+      { field: "mission" as const },
+      { field: "company" as const, cellRenderer: CompanyLogoRenderer },
+      { field: "location" as const },
+      { field: "date" as const },
+      {
+        field: "price" as const,
+        valueFormatter: (params: any) => `£${params.value.toLocaleString()}`,
+      },
+      { field: "successful" as const },
+      { field: "rocket" as const },
     ]);
-  
-    const defaultColDef = {
-      flex: 1,
-    };
-  
+
+    useEffect(() => {
+      fetch("https://www.ag-grid.com/example-assets/space-mission-data.json")
+        .then((result) => result.json())
+        .then((rowData) => setRowData(rowData));
+    }, []);
+
+    const defaultColDef = useMemo(
+      () => ({
+        filter: true,
+        flex: 1,
+      }),
+      []
+    );
+
+    const rowSelection = useMemo(
+      () => ({
+        mode: "multiRow" as const,
+      }),
+      []
+    );
+
     return (
       <div style={{ height: "100vh" }}>
         <AgGridReact
           rowData={rowData}
           columnDefs={colDefs}
           defaultColDef={defaultColDef}
+          pagination={true}
+          onCellValueChanged={(event) => {
+            console.log(`New Cell Value: ${event.value};`);
+          }}
+          rowSelection={rowSelection}
+          onRowSelected={(event) => {
+            console.log(event.api.getSelectedRows());
+          }}
         />
       </div>
     );
