2d6b78dcaea7fbe836e21d59bec37444c2e55ace
Nelson Boralli Neto <netoboralli@gmail.com>
2025-08-16 16:12:33 -0300

    move aggrid to index path

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-  route("aggrid", "routes/aggrid.tsx"),
+  index("routes/aggrid.tsx"),
+  route("home", "routes/home.tsx"),
 ] satisfies RouteConfig;
